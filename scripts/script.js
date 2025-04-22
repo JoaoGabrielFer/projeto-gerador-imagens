94770193359dbb7276d1b8a84536dc1f4a20e7dc
JoaoGabrielFer <joaoggabriel07@gmail.com>
2025-04-22 11:45:30 -0300

    versão quase final pra av1 do projeto

---
+++ scripts/script.js
@@ -0,0 +1,36 @@
+function img1add(){
+    document.getElementById("img1").classList.add("showimg1")
+}
+function img1remove(){
+    document.getElementById("img1").classList.remove("showimg1")
+}
+function img2add(){
+    document.getElementById("img2").classList.add("showimg2")
+}
+function img2remove(){
+    document.getElementById("img2").classList.remove("showimg2")
+}
+function img3add(){
+    document.getElementById("img3").classList.add("showimg3")
+}
+function img3remove(){
+    document.getElementById("img3").classList.remove("showimg3")
+}
+function img4add(){
+    document.getElementById("img4").classList.add("showimg4")
+}
+function img4remove(){
+    document.getElementById("img4").classList.remove("showimg4")
+}
+function img5add(){
+    document.getElementById("img5").classList.add("showimg5")
+}
+function img5remove(){
+    document.getElementById("img5").classList.remove("showimg5")
+}
+function img6add(){
+    document.getElementById("img6").classList.add("showimg6")
+}
+function img6remove(){
+    document.getElementById("img6").classList.remove("showimg6")
+}
